f51e913af600209f3a30eba643cae55e20d741b1
maszulc <maszulcx@gmail.com>
2022-11-09 14:04:34 +0100

    adjust variable name

--- sliding_window/max_profit.py
+++ sliding_window/max_profit.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        res = 0
+        result = 0
 
         l = 0
         for r in range(1, len(prices)):
             if prices[r] < prices[l]:
                 l = r
-            res = max(res, prices[r] - prices[l])
-        return res
+            result = max(result, prices[r] - prices[l])
+        return result
