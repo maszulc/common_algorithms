04a11680022f9fa39a26c698dfca4891af9dfda2
maszulc <maszulcx@gmail.com>
2022-11-10 18:11:52 +0100

    adjustments_for maxprofit and bubble_sort

--- Sorting/bubble-sort.py
+++ Sorting/bubble-sort.py
@@ -1,5 +1,5 @@
 class Solution():
-    def bubbleSort(self,nums:list[int]) -> list[int]:
+    def bubbleSort(self, nums: list[int]) -> list[int]:
         for i in range(len(nums)):
             for j in range(len(nums) - 1):
                 if nums[j] > nums[j+1]:
@@ -7,8 +7,8 @@ class Solution():
                     nums[j+1] = nums[j]
                     nums[j] = tmp
         return nums
-    
-    def bubbleSort2(self, nums:list[int]) -> list[int]:
+
+    def bubbleSort2(self, nums: list[int]) -> list[int]:
         listLength = len(nums)
         for _ in range(listLength):
             for i in range(listLength - 1):
@@ -16,7 +16,8 @@ class Solution():
                     nums[i], nums[i + 1] = nums[i + 1], nums[i]
         return nums
 
-arr = [4,2,6,1]
+
+arr = [4, 2, 6, 1, 8, 6, 5]
 test = Solution()
 print(test.bubbleSort(arr))
 print(test.bubbleSort2(arr))
--- sliding_window/max_profit.py
+++ sliding_window/max_profit.py
@@ -2,10 +2,15 @@ class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         result = 0
 
-        l = 0
-        for r in range(1, len(prices)):
-            if prices[r] < prices[l]:
-                l = r
-            result = max(result, prices[r] - prices[l])
+        left_pointer = 0
+        for right_pointer in range(1, len(prices)):
+            if prices[right_pointer] < prices[left_pointer]:
+                left_pointer = right_pointer
+            result = max(result, prices[right_pointer] - prices[left_pointer])
         return result
-        
+
+
+prices = [7, 1, 5, 3, 6, 4]
+test = Solution()
+max_profit = test.maxProfit(prices)
+print(max_profit)
