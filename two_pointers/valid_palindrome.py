b86d2d0c9d651f5f256f55c7639ac52f35c07e2a
maszulc <maszulcx@gmail.com>
2022-11-12 19:34:30 +0000

    edited valid_palindrome.py

--- two_pointers/valid_palindrome.py
+++ two_pointers/valid_palindrome.py
@@ -2,16 +2,18 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         lp, rp = 0, len(s) - 1
         while lp < rp:
-            if not s[lp].isalnum():
-                lp +=1
-            if not s[rp].isalnum():
+            # skip non alfanumeric characters
+            while not s[lp].isalnum() and lp < rp:
+                lp += 1
+            while not s[rp].isalnum() and lp < rp:
                 rp -= 1
+            # print(f'compare if {s[lp]} == {s[rp]}:')
             if s[lp].lower() != s[rp].lower():
                 return False
-            lp, rp = lp + 1, rp -1
+            lp, rp = lp + 1, rp - 1
         return True
 
 
-test_string = "kajaK"
+test_string = "A man, a plan, a canal: Panama"
 testobject = Solution()
 print(testobject.isPalindrome(test_string))
